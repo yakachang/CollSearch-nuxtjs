1b630a84331b06746bc1577b52425e87b7dd657b
yichen <yichen@nlplab.cc>
2022-01-19 19:46:57 +0800

    feat: Update index

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,53 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container fill-height fluid style="height: 100vh">
+    <v-row 
+      align="center"
+      justify="center"
+    >
+      <v-col>
+        <v-text-field
+          v-model="query"
+          placeholder="Linggle Search"
+          append-icon="mdi-help"
+          class="mt-3"
+          rounded
+          solo
+          @keydown.enter="search(query)"
+          @click:append="linggleHelp"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      query: "",
+    }
+  },
+  head() {
+    return {
+      title: 'Collocation Search',
+    }
+  },
+  methods: {
+    async search(query) {
+      // console.log(query)
+      // let result = this.$axios.$get('/ngram/', {query: query})
+      let result = this.$axios.$get(
+        `/`
+      ).catch(error => {
+        console.log(error)
+      })
+      // console.log('result: ', result)
+      // return result
+    },
+    linggleHelp() {
+      window.open('https://linggle.com/help/')
+    }
+  },
 }
 </script>
