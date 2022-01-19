1fcaf49f6786c0838ddbcb1282200623b8fef937
yichen <yichen@nlplab.cc>
2022-01-19 22:17:34 +0800

    feat: Set theme configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,8 +56,18 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
+        light: {
+          primary: '#1976D2',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+          background: '#3a405a',
+        },
         dark: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
