7353107fa6413833ddd3ed12a589ade55509f6d4
yichen <yichen@nlplab.cc>
2022-01-19 22:18:38 +0800

    feat: Set backgraound color in index and show search result

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fill-height fluid style="height: 100vh">
+  <v-container fill-height fluid style="height: 100%">
     <v-row 
       align="center"
       justify="center"
@@ -17,6 +17,36 @@
         ></v-text-field>
       </v-col>
     </v-row>
+    <v-card
+      v-show="res_list.length !== 0"
+      class="mx-auto"
+      width="100vw"
+    >
+      <v-list>
+        <v-list-group
+          v-for="(res, idx) in res_list"
+          :key="idx"
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="`${res[0]}`"></v-list-item-title>
+              <v-list-item-subtitle v-text="`Count: ${res[1]} (${res[2]}%)`"></v-list-item-subtitle>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item>
+            <v-list-item-content>
+              <ul
+                v-for="(example, idx) in example_list"
+                :key="idx"
+              >
+                <li>{{ example }}</li>
+              </ul>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-card>
   </v-container>
 </template>
 
@@ -26,6 +56,8 @@ export default {
   data() {
     return {
       query: "",
+      res_list: [],
+      example_list: [],
     }
   },
   head() {
@@ -42,6 +74,8 @@ export default {
       ).catch(error => {
         console.log(error)
       })
+
+      this.res_list = result["res"].slice(0, 10)
       // console.log('result: ', result)
       // return result
     },
