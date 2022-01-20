eebd4d49e30cef4d7a6379fbe5ffe5738c6a4d6d
yichen <yichen@nlplab.cc>
2022-01-20 10:38:46 +0800

    chore: Separate index page and search page

---
+++ pages/search.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-container fill-height fluid style="height: 100%">
+    <v-row 
+      align="center"
+      justify="center"
+    >
+      <v-col>
+        <v-text-field
+          v-model="query"
+          placeholder="Collocation Search"
+          append-icon="mdi-help"
+          class="mt-3"
+          rounded
+          solo
+          @keydown.enter="search(query)"
+          @click:append="linggleHelp"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-card
+      v-show="result_list.length !== 0"
+      class="mx-auto"
+      width="100vw"
+    >
+      <v-list>
+        <v-list-group
+          v-for="(result, idx) in result_list"
+          :key="idx"
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title v-text="`${result.res[0]}`"></v-list-item-title>
+              <v-list-item-subtitle v-text="`Count: ${result.res[1]} (${result.res[2]}%)`"></v-list-item-subtitle>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item>
+            <v-list-item-content>
+              <ul
+                v-for="(example, idx) in result.example.slice(0,10)"
+                :key="idx"
+              >
+                <li>{{ example }}</li>
+              </ul>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      query: "",
+      res_list: [],
+      example_list: [],
+      result_list: [],
+    }
+  },
+  head() {
+    return {
+      title: 'Collocation Search',
+    }
+  },
+  methods: {
+    async search(query) {
+      let result = await this.$axios.$post(
+        `/search/${query}`, {query: query}
+      ).catch(error => {
+        console.log(error)
+      })
+
+      this.result_list = result.slice(0, 10)
+    },
+    linggleHelp() {
+      window.open('https://linggle.com/help/')
+    }
+  },
+}
+</script>
