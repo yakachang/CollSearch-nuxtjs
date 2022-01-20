1a39e547e34d3e5052ecbb7ef3ea64cfab36ba0f
yichen <yichen@nlplab.cc>
2022-01-20 18:57:06 +0800

    75b7790

---
+++ _nuxt/static/1642676222/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FCollSearch-nuxtjs\u002F_nuxt\u002Fstatic\u002F1642676222",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FCollSearch-nuxtjs\u002F",assetsPath:"\u002FCollSearch-nuxtjs\u002F_nuxt\u002F",cdnURL:a}}}}(null));
